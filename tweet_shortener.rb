7a9a4702c23bf3ad3571a73c59776df35a0d8e6d
Nicholas  Luong <nckluong@gmail.com>
2018-08-26 23:05:31 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,4 +1,16 @@
 # Write your code here.
 
 def dictionary
-  dictionary => { :hello => "hi", :
+ dictionary = {
+		"hello" => 'hi',
+		"to" => '2',
+		"two" => '2',
+		"too" => '2',
+		"for" => '4',
+		"four" => '4',
+		"be" => 'b',
+		"you" => 'u',
+		"at" => '@',
+		"and" => '&'
+	}
+end
