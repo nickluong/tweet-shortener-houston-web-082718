3e613dec6c1c8bc5b2ad07edde98fdc459a31ad5
Nicholas  Luong <nckluong@gmail.com>
2018-08-26 23:10:58 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -26,3 +26,30 @@ def word_substituter(tweet)
 		end
 	end .join(" ")
 end 
+
+
+def bulk_tweet_shortener(tweets)
+	tweets.each {|tweet| puts word_substituter(tweet)}
+end
+
+
+def selective_tweet_shortener(tweet)
+	if tweet.length > 140
+		word_substituter(tweet)
+	else
+		tweet
+	end
+end
+
+
+def shortened_tweet_truncator(tweet)
+	if tweet.length > 140
+		while tweet.length > 140 do
+			tweet1 = tweet[0..136] + "..."
+			tweet = tweet[137..tweet.length]
+		end
+		tweet1
+	else
+		tweet
+	end
+end
